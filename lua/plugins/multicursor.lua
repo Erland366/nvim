07dc62c6ff5d8d8a64d703afb6d56f58f7567c12
Erland366 <erland.pg366@gmail.com>
2024-07-14 01:11:37 +0700

    fix multicursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,3 +1,18 @@
 return {
-  "mg979/vim-visual-multi",
+  {
+    "mg979/vim-visual-multi",
+    config = function()
+      vim.api.nvim_set_keymap("x", "<C-d>", "<Plug>(VM-Find-Subword-Under)", {
+        noremap = true,
+        silent = true,
+        desc = "Select multiple highlighted word/letter",
+      })
+
+      vim.api.nvim_set_keymap("n", "<C-d>", "<Plug>(VM-Find-Under)", {
+        noremap = true,
+        silent = true,
+        desc = "Select multiple highlighted word/letter",
+      })
+    end,
+  },
 }
