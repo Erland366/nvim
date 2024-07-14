8d95957f72bfe4c6975faa71f093939179bb50ed
Erland366 <erland.pg366@gmail.com>
2024-07-15 01:13:46 +0700

    fix telescope

--- lua/plugins/telescopes.lua
+++ lua/plugins/telescopes.lua
@@ -1,13 +1,13 @@
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<C-p>",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<C-p>",
+      LazyVim.pick("files"),
+      desc = "Find Files (Root Dir)",
+    },
     {
       "<C-O>",
       function()
